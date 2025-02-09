5b06b5b554934c1583f44012f2fe233d8d96698c
jnywong <jnywong.pro@gmail.com>
2025-02-09 14:22:00 +0000

    Set up homepage

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,9 +1,39 @@
-import { useState } from 'react';
+import Logo from './ui/logo';
+import Image from 'next/image';
 
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
 
 export default function HomePage() {
-  // ...
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-orange-500 p-4 md:h-52">
+        <Logo />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Prepare in advance.</strong> Scale up resources for your{' '}
+            <em className="text-orange-500">JupyterHub</em>, ahead of time.
+          </p>
+          {/* <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link> */}
+        </div>
+        <div className="flex items-center justify-center">
+          <Image
+            src="/hero-desktop.jpg"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Hero image"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ frontend/src/app/ui/logo.tsx
@@ -0,0 +1,10 @@
+import { FireIcon } from '@heroicons/react/24/outline';
+
+export default function AcmeLogo() {
+  return (
+    <div className={`flex flex-row items-center leading-none text-white`}>
+      <p className="text-[44px]">Cluster Warmer</p>
+      <FireIcon className="h-12 w-12 rotate-[15deg]" />
+    </div>
+  );
+}
