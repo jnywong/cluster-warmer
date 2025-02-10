fb0b0e21449284e58537898d612da5eb813a19cd
jnywong <jnywong.pro@gmail.com>
2025-02-10 10:48:34 +0000

    Add custom action to post request

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,14 +1,23 @@
 from django.urls import include, path
 from rest_framework import routers
-
-from .views import ClusterViewSet, EventViewSet, IndexView
+from web.views import ClusterViewSet, EventViewSet, IndexView
 
 router = routers.DefaultRouter()
 router.register(r"cluster", ClusterViewSet)
-router.register(r"events", EventViewSet)
+# router.register(r"events", EventViewSet)
+
+# Custom binding for EventViewSet
+
+event_list = EventViewSet.as_view({"get": "list", "post": "create_and_submit"})
+
+event_detail = EventViewSet.as_view(
+    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("api/", include(router.urls)),
+    path("api/events/", event_list, name="event-list"),
+    path("api/events/<int:pk>/", event_detail, name="event-detail"),
 ]
